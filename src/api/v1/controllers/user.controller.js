b06ac4df4b88c6845d77ff1fb1a3b96ba96fe73c
Adi Kurniawan <adikurniawan.dev@gmail.com>
2022-12-28 17:27:44 +0700

    feat(user): update password

--- src/api/v1/controllers/user.controller.js
+++ src/api/v1/controllers/user.controller.js
@@ -1,7 +1,7 @@
 "use strict";
 const crypto = require("crypto");
 const { sequelize, User, UserBiodata } = require("../models");
-const { hashPassword, generateJWT } = require("../helpers");
+const { hashPassword, generateJWT, comparePassword } = require("../helpers");
 
 class UserController {
   static async register(req, res, next) {
@@ -29,6 +29,48 @@ class UserController {
         .json({ message: "User created successfully", data: { token } });
     } catch (error) {
       await transaction.rollback();
+      next(error);
+    }
+  }
+
+  static async updatePassword(req, res, next) {
+    try {
+      const { oldPassword, verificationPassword, newPassword } = req.body;
+
+      if (verificationPassword !== newPassword) {
+        throw {
+          status: 422,
+          message: "New password and verification password do not match",
+        };
+      }
+
+      const user = await User.findOne({
+        attributes: ["id", "password"],
+        where: {
+          id: req.user.id,
+        },
+      });
+
+      if (user && (await comparePassword(oldPassword, user.password))) {
+        await User.update(
+          {
+            password: await hashPassword(newPassword),
+          },
+          {
+            where: {
+              id: user.id,
+            },
+          }
+        );
+
+        res.status(200).json({ message: "Password updated successfully" });
+      } else {
+        throw {
+          status: 422,
+          message: "Invalid old password",
+        };
+      }
+    } catch (error) {
       next(error);
     }
   }
